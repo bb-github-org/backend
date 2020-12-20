c86baa3985a139141bf6e3c29a560b06922d6230
Burak Direk <bkdir@Buraks-MBP.fios-router.home>
2020-12-19 20:24:35 -0500

    Connect to PostgreSQL and fetch some data(working)

--- application.go
+++ application.go
@@ -3,58 +3,58 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "bkdir"
-	password = "password"
-	dbname   = "go_backend_development"
+var (
+	host      = os.Getenv("RDS_HOSTNAME")           //localhost
+	port, err = strconv.Atoi(os.Getenv("RDS_PORT")) //5432
+	user      = os.Getenv("RDS_USERNAME")           //postgres
+	password  = os.Getenv("RDS_PASSWORD")           //password
+	dbname    = os.Getenv("DB_NAME")                //go_backend_development
 )
 
 func main() {
 	db := connect()
-	fmt.Println("db")
-	fmt.Println(db)
 
-	rows, err := db.Query("SELECT user_name FROM public.users LIMIT 10")
-	fmt.Println("rows:")
-	fmt.Println(rows)
+	rows, err := db.Query("SELECT user_name FROM users LIMIT 10;")
 	if err != nil {
 		fmt.Printf("failed to execute query. Error: %s", err)
 	}
 	defer rows.Close()
 
+	var usernames []string
 	for rows.Next() {
 		var username string
 		err = rows.Scan(&username)
 		if err != nil {
 			fmt.Printf("failed to scan rows. Error: %s", err)
 		}
-		fmt.Println(username)
+		usernames = append(usernames, username)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+		fmt.Fprintf(w, "%s", usernames)
 	})
-	//log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
 func connect() *sql.DB {
 	psqlInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable connect_timeout=5",
 		host, port, user, password, dbname,
 	)
+	fmt.Println(psqlInfo)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
