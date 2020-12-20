e38eb5fe718ee93e78672a3650077787de3b4455
Burak Direk <bkdir@Buraks-MBP.fios-router.home>
2020-12-19 19:35:12 -0500

    Connect to PostgreSQL and fetch some rows(broken)

--- application.go
+++ application.go
@@ -1,16 +1,65 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "bkdir"
+	password = "password"
+	dbname   = "go_backend_development"
+)
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	db := connect()
+	fmt.Println("db")
+	fmt.Println(db)
+
+	rows, err := db.Query("SELECT user_name FROM public.users LIMIT 10")
+	fmt.Println("rows:")
+	fmt.Println(rows)
+	if err != nil {
+		fmt.Printf("failed to execute query. Error: %s", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var username string
+		err = rows.Scan(&username)
+		if err != nil {
+			fmt.Printf("failed to scan rows. Error: %s", err)
+		}
+		fmt.Println(username)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	})
+	//log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func connect() *sql.DB {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
